e98a2d786b6f8e01a2e4e6e85732cf6e12144ce1
SimoAxel <axel.simokengne@aizoongroup.com>
2024-03-29 14:58:29 +0100

    Mdn "My shopplist chalenge" completed

---
+++ shopping_list_challenge/script.js
@@ -0,0 +1,48 @@
+const inputNode = document.querySelector("input");
+const addBtn = document.getElementById("add-btn");
+
+window.addEventListener('click', () => {
+    inputNode.focus();
+});
+
+window.addEventListener('click', () => {
+    inputNode.focus();
+});
+
+let newListElement;
+
+function deleteElement(node) {
+    const ulist = document.querySelector("ul");
+    ulist.removeChild(node);
+}
+
+addBtn.onclick = function() {
+    if(inputNode.value !== "") {
+        newListElement = inputNode.value;
+        inputNode.value = "";
+
+        // I select the unordered list
+        const ulist = document.querySelector("ul");
+
+        // I creat the list item, add the name of the item
+        // and the button that will go with it
+        const newEl = document.createElement("li");
+        const newElBtn = document.createElement("button")
+        const buttonText = document.createTextNode("Delete");
+        const text = document.createTextNode(newListElement);
+
+        newElBtn.addEventListener('click', () => {
+            ulist.removeChild(newEl);
+            inputNode.focus();
+        });
+        // I append all the elements together
+        newElBtn.appendChild(buttonText);
+        newEl.appendChild(text);
+        newEl.appendChild(newElBtn);
+        ulist.appendChild(newEl);
+
+        newElBtn.style.margin = "10px";
+
+        inputNode.focus();
+    }
+}
